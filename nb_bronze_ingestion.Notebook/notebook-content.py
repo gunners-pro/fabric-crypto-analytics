fb6fad464182e5e4f308589c77509b39a25533e8
Fabricyo Barreto <bass.barreto@gmail.com>
2025-12-18 21:34:53 -0300

    chore: initial lakehouse and bronze ingestion notebook

---
+++ nb_bronze_ingestion.Notebook/notebook-content.py
@@ -0,0 +1,32 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "fe6ebbc7-584b-4dfa-8551-2e146c29865b",
+# META       "default_lakehouse_name": "lh_crypto",
+# META       "default_lakehouse_workspace_id": "cf249472-6085-4a46-9436-f2a2075428d1",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "fe6ebbc7-584b-4dfa-8551-2e146c29865b"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+print("Bronze ingestion notebook created")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
